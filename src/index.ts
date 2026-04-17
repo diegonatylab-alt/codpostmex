d5b332b86bdeb71da5ceef23b03e91006c0e3bb5
diegonatylab <diegonatylab@gmail.com>
2026-04-17 15:15:54 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -43,7 +43,7 @@ app.use('*', async (c, next) => {
 app.use(
   '*',
   cache({
-    cacheName: 'buscarcpmexico-v14',
+    cacheName: 'buscarcpmexico-v15',
     cacheControl: 'public, max-age=86400, s-maxage=86400',
   })
 );
